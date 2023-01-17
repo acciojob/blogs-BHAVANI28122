87cf74dd10b66cf86f6d149c415a88ae7549db9d
BHAVANI28122 <bhavanigavala@gmail.com>
2023-01-17 10:36:24 +0530

    push

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -46,8 +46,36 @@ public class ImageService {
     public int countImagesInScreen(Image image, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
         //In case the image is null, return 0
-        int count =0;
 
-       return count;
+        String dimensions = image.getDimensions();
+        int xi = 0;
+        int yi = 0;
+        int xs = 0;
+        int ys = 0;
+        int num = 0;
+        for(int i = 0; i<dimensions.length(); i++){
+            if(dimensions.charAt(i) == 'X'){
+                xi = num;
+                num = 0;
+                continue;
+            }
+            num *= 10;
+            num += (dimensions.charAt(i) - '0');
+        }
+        yi = num;
+        num = 0;
+        for(int i = 0; i<screenDimensions.length(); i++){
+            if(screenDimensions.charAt(i) == 'X'){
+                xs = num;
+                num = 0;
+                continue;
+            }
+            num *= 10;
+            num += (screenDimensions.charAt(i) - '0');
+        }
+        ys = num;
+
+        int ans = (int) ((int) (Math.floor((xs)/(xi)))*(Math.floor((ys)/(yi))));
+        return ans;
     }
 }
